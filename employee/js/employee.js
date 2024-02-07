f844b278e20c2835f50bc09974c5cfa6a0fc24c1
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-02-07 14:32:00 +0530

    employee 3 options are updated

--- employee/js/employee.js
+++ employee/js/employee.js
@@ -1,10 +1,32 @@
 const sidemenu = document.querySelector(".sidemenu");
 const toggle_sidemenu = document.querySelector(".toggle_sidemenu");
 const main = document.querySelector(".main");
+const all_sections = document.querySelectorAll(".section");
+const all_click = document.querySelectorAll(".click");
 
+// toggle sidemenu
 toggle_sidemenu.addEventListener("click",()=>{
     sidemenu.classList.toggle("active");
     toggle_sidemenu.classList.toggle("active");
     main.classList.toggle("active");
 });
 
+// display respective sections
+all_click.forEach(click => {
+    click.addEventListener("click", ()=>{
+        let index = click.getAttribute("index");
+        document.querySelector(".current_path_name").innerHTML = click.textContent.trim();
+    all_sections.forEach(section => {
+        let sectionIndex = section.getAttribute("index");
+        if (index == sectionIndex) {
+            section.style.display = "block";
+        }else{
+            section.style.display = "none";
+        }
+    });
+    all_click.forEach(btn => {
+        btn.classList.remove("active");
+    });
+    click.classList.add("active");
+    });
+});
