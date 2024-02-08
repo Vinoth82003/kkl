3d01bdaf262f494ebd580cb9c1ffbdcf8b163805
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-02-08 15:25:06 +0530

    edit changes

--- employee/js/employee.js
+++ employee/js/employee.js
@@ -8,6 +8,19 @@ const user = {
 
 }
 
+const oldData = {
+    
+}
+
+oldData.oldname = document.querySelector(".uname").textContent.trim();
+oldData.oldgender = document.querySelector(".ugender").textContent.trim();
+oldData.oldemail = document.querySelector(".uemail").textContent.trim();
+oldData.oldcontact = document.querySelector(".ucontact").textContent.trim();
+oldData.oldaddress = document.querySelector(".uaddress").textContent.trim();
+oldData.olddoj = document.querySelector(".udoj").textContent.trim();
+
+console.log(oldData);
+
 let uId = document.querySelector(".uid").textContent.trim();
 user.empId = uId;
 
@@ -52,20 +65,10 @@ if (lastClick != null) {
     showSection(lastClick);
 }
 
-const all_input = document.querySelectorAll(".input");
 const all_btn = document.querySelectorAll(".submit");
 const all_cancel = document.querySelectorAll(".cancel");
 const all_field = document.querySelectorAll(".input_field");
-
-all_input.forEach(input => {
-    input.addEventListener("click", () => {
-        input.removeAttribute("readonly");
-        let type = (input.getAttribute("name"));
-        user.type = type;
-        user.old = input.value;
-        input.parentElement.classList.add("active")
-    });
-});
+const all_input = document.querySelectorAll(".input");
 
 all_btn.forEach(btn => {
     btn.addEventListener("click", ()=>{
@@ -116,19 +119,41 @@ document.querySelector(".model_close").addEventListener("click", ()=>{
 
 message()
 
+
+all_field.forEach(field => {
+    field.addEventListener("click", ()=>{
+        all_field.forEach(fld => {
+            let input = fld.querySelector(".input");
+            if (input) {
+                input.setAttribute("readonly",true)
+            }
+            fld.classList.remove("active")
+        });
+        field.querySelector(".input").removeAttribute("readonly");
+        field.classList.add("active")
+    });
+
+});
+
+
 all_cancel.forEach(btn => {
     btn.addEventListener("click", ()=>{
+        let input = btn.previousElementSibling.previousElementSibling;
+        
+        input.value = user.old;
+
         btn.previousElementSibling.setAttribute("readonly",true);
         btn.parentElement.classList.remove("active");
-
-        let input = btn.previousElementSibling.previousElementSibling;
-        input.value = user.old;
+        setTimeout(() => {
+            btn.parentElement.classList.remove("active");
+        }, 50);
     });
 });
 
-all_field.forEach(field => {
-    field.addEventListener("blur", ()=>{
-        field.querySelector("input").setAttribute("readonly",true);
-        field.classList.remove("active")
+all_input.forEach(input => {
+    input.addEventListener("click", ()=>{
+        user.old = input.value;
+        user.type = input.getAttribute("name");
+        console.log(user);
     });
 });
