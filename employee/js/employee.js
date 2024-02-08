f7fb48caa2b48ec4ca093c3de6109b048ebcb446
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-02-08 10:45:20 +0530

    adding js conditions

--- employee/js/employee.js
+++ employee/js/employee.js
@@ -4,6 +4,13 @@ const main = document.querySelector(".main");
 const all_sections = document.querySelectorAll(".section");
 const all_click = document.querySelectorAll(".click");
 
+const user = {
+
+}
+
+let uId = document.querySelector(".uid").textContent.trim();
+user.empId = uId;
+
 // toggle sidemenu
 toggle_sidemenu.addEventListener("click",()=>{
     sidemenu.classList.toggle("active");
@@ -16,6 +23,13 @@ all_click.forEach(click => {
     click.addEventListener("click", ()=>{
         let index = click.getAttribute("index");
         document.querySelector(".current_path_name").innerHTML = click.textContent.trim();
+        showSection(index)
+        localStorage.setItem("lastClick",index);
+    // click.classList.add("active");
+    });
+});
+
+function showSection(index) {
     all_sections.forEach(section => {
         let sectionIndex = section.getAttribute("index");
         if (index == sectionIndex) {
@@ -25,16 +39,52 @@ all_click.forEach(click => {
         }
     });
     all_click.forEach(btn => {
-        btn.classList.remove("active");
+        if (index == btn.getAttribute("index")) {
+            btn.classList.add("active");
+        }else{
+            btn.classList.remove("active");
+        }
     });
-    click.classList.add("active");
-    });
-});
+}
+
+let lastClick = localStorage.getItem("lastClick");
+if (lastClick != null) {
+    showSection(lastClick);
+}
 
 const all_input = document.querySelectorAll(".input");
+const all_btn = document.querySelectorAll(".submit");
+const all_cancel = document.querySelectorAll(".cancel");
+const all_field = document.querySelectorAll(".input_field");
 
 all_input.forEach(input => {
     input.addEventListener("click", () => {
         input.removeAttribute("readonly");
+        let type = (input.getAttribute("name"));
+        user.type = type;
+        user.old = input.value;
+        input.parentElement.classList.add("active")
+    });
+});
+
+all_btn.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        let newvalue = btn.previousElementSibling.value
+        user.new = (newvalue);
+        console.log(user);
+    })
+});
+
+all_cancel.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        btn.previousElementSibling.setAttribute("readonly",true);
+        btn.parentElement.classList.remove("active")
+    });
+});
+
+all_field.forEach(field => {
+    field.addEventListener("blur", ()=>{
+        field.querySelector("input").setAttribute("readonly",true);
+        field.classList.remove("active")
     });
 });
