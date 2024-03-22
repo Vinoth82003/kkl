e0d03da9eeeec3f47cca4b832781cd876ce23524
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-03-22 08:30:59 +0530

    search push

--- js/search.js
+++ js/search.js
@@ -2,15 +2,26 @@ let all_serac_card = document.querySelectorAll(".serac_card");
 let searchBar = document.querySelector(".empSearch");
 
 searchBar.addEventListener("input", () => {
-  let value = searchBar.value;
+  let value = searchBar.value.trim(); // Trim whitespace from the input value
 
   all_serac_card.forEach((card) => {
     let id = card.getAttribute("data-id");
-    console.log(id);
-    if (value == id) {
-      card.style.display = "flex";
+    let nameAttribute = card.getAttribute("data-name");
+
+    if (isNaN(value)) {
+      // Check if value is not a number
+      if (nameAttribute.includes(value)) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
     } else {
-      card.style.display = "none";
+      if (value === id) {
+        // Use strict equality comparison to compare numbers
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
     }
   });
 });
