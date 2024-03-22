d77ef8c54c08ee3d7fcb7c9587365b5f082dcb68
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-03-22 09:56:38 +0530

    push

--- js/search.js
+++ js/search.js
@@ -13,3 +13,28 @@ okBtn.addEventListener("click", () => {
     alert("fill two date fields");
   }
 });
+
+document
+  .querySelector(".donwload_search")
+  .addEventListener("click", function () {
+    // Select the chart container element
+    const chartContainer = document.querySelector(".searchEmployeeSection");
+    let fileName = "Employee Detail";
+    // Use html2canvas to capture the content of the chart container
+    html2canvas(chartContainer).then((canvas) => {
+      // Convert the canvas to a data URL
+      const dataURL = canvas.toDataURL();
+
+      // Create a temporary link element
+      const link = document.createElement("a");
+      link.href = dataURL;
+      link.download = `${fileName}.png`; // Set the download filename
+
+      // Append the link to the document and trigger a click event to download the image
+      document.body.appendChild(link);
+      link.click();
+
+      // Remove the temporary link from the document
+      document.body.removeChild(link);
+    });
+  });
