a9250f92ac159c85eae52cf09e4bfe4a2e854d72
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-03-22 09:46:45 +0530

    filter validate added

--- js/search.js
+++ js/search.js
@@ -1,27 +1,15 @@
-let all_serac_card = document.querySelectorAll(".serac_card");
-let searchBar = document.querySelector(".empSearch");
+let fromDate = document.getElementById("fromdate");
+let toDate = document.getElementById("todate");
 
-searchBar.addEventListener("input", () => {
-  let value = searchBar.value.trim(); // Trim whitespace from the input value
+let okBtn = document.querySelector(".okBtn");
 
-  all_serac_card.forEach((card) => {
-    let id = card.getAttribute("data-id");
-    let nameAttribute = card.getAttribute("data-name");
-
-    if (isNaN(value)) {
-      // Check if value is not a number
-      if (nameAttribute.includes(value)) {
-        card.style.display = "flex";
-      } else {
-        card.style.display = "none";
-      }
-    } else {
-      if (value === id) {
-        // Use strict equality comparison to compare numbers
-        card.style.display = "flex";
-      } else {
-        card.style.display = "none";
-      }
-    }
-  });
+okBtn.addEventListener("click", () => {
+  if (
+    (fromDate.value.length > 0 && toDate.value.length > 0) ||
+    (fromDate.value.length <= 0 && toDate.value.length <= 0)
+  ) {
+    alert("send to backend");
+  } else {
+    alert("fill two date fields");
+  }
 });
